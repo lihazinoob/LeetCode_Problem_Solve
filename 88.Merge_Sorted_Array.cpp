a8bf947be8e23cced0f76efd87a1ac46918326dd
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-08-11 19:01:30 +0600

    Accepted YEEEE.

--- 88.Merge_Sorted_Array.cpp
+++ 88.Merge_Sorted_Array.cpp
@@ -8,16 +8,23 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
     {
       vector<int>third;
-      for(int i = 0;i<m;i++)
+      int index;
+      for(int i = m-1;i>=0;--i)
       {
         if(nums1[i] != 0)
         {
+            index = i;
+            break;
+        }
+        
+      }
+      for(int i = 0;i<=index;i++)
+      {
+        
+        
           third.push_back(nums1[i]);
-        }
-        else
-        {
-          continue;
-        }
+        
+        
       }
       for(int i = 0;i<n;++i)
       {
